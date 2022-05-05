388041b0e732b770071c915f05033bdb61eb3bed
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-05 10:29:12 +0300

    feat: создать класс ValidationException

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.exception;
+
+/**
+ * Класс исключения, которое будет генерироваться в случае если данные для создания нового фильма или пользователя -
+ * некорректны.
+ */
+public class ValidationException extends Exception {
+    public ValidationException(String msg) {
+        super(msg);
+    }
+}
