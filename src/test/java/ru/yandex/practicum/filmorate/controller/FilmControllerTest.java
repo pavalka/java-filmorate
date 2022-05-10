100e191079d54c8c0f72be457e4301ac44d2ebd3
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-10 19:21:31 +0300

    refactor: заменить в FilmControllerTest.java import static org.junit.jupiter.api.Assertions.* на импорт каждого использованного метода style: убрать в FilmControllerTest перенос строки в строках, где код умещается в 120 сиволов

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -8,7 +8,10 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FilmControllerTest {
     private FilmController filmController;
@@ -26,8 +29,7 @@ class FilmControllerTest {
     @Test
     void getAllFilmsShouldReturnFilmsCollectionWhenOneFilmIsAdded() {
         try {
-            filmController.addFilm(new Film("Test film", "Film description", LocalDate.of(2001,
-                    2, 14), 90));
+            filmController.addFilm(new Film("Test film", "Film description", LocalDate.of(2001, 2, 14), 90));
         } catch (ValidationException ex) {
             System.out.println(ex.getMessage());
         }
@@ -37,8 +39,7 @@ class FilmControllerTest {
 
     @Test
     void addFilmShouldThrowExceptionWhenAddNull() {
-        assertThrows(ValidationException.class,
-                                        () -> filmController.addFilm(null));
+        assertThrows(ValidationException.class, () -> filmController.addFilm(null));
         assertTrue(filmController.getAllFilms().isEmpty());
     }
 
@@ -52,24 +53,21 @@ class FilmControllerTest {
 
     @Test
     void addFilmShouldThrowExceptionWhenAddFilmWithEmptyDescription() {
-        Film film = new Film("Test film", "", LocalDate.of(2001,
-                      2, 14), 90);
+        Film film = new Film("Test film", "", LocalDate.of(2001, 2, 14), 90);
         assertThrows(ValidationException.class, () -> filmController.addFilm(film));
         assertTrue(filmController.getAllFilms().isEmpty());
     }
 
     @Test
     void addFilmShouldThrowExceptionWhenAddFilmWithDescriptionLengthIs201() {
-        Film film = new Film("Test film", "Description".repeat(18) + "End", LocalDate.of(2001,
-                      2, 14), 90);
+        Film film = new Film("Test film", "Description".repeat(18) + "End", LocalDate.of(2001, 2, 14), 90);
         assertThrows(ValidationException.class, () -> filmController.addFilm(film));
         assertTrue(filmController.getAllFilms().isEmpty());
     }
 
     @Test
     void addFilmShouldThrowExceptionWhenAddFilmWithWrongDate() {
-        Film film = new Film("Test film", "Description", LocalDate.of(1895, 12,
-                  17), 90);
+        Film film = new Film("Test film", "Description", LocalDate.of(1895, 12, 17), 90);
         assertThrows(ValidationException.class, () -> filmController.addFilm(film));
         assertTrue(filmController.getAllFilms().isEmpty());
     }
@@ -83,16 +81,14 @@ class FilmControllerTest {
 
     @Test
     void addFilmShouldThrowExceptionWhenAddFilmWithNegativeDuration() {
-        Film film = new Film("Test Film", "Description", LocalDate.of(2001, 2, 14),
-                             -1);
+        Film film = new Film("Test Film", "Description", LocalDate.of(2001, 2, 14), -1);
         assertThrows(ValidationException.class, () -> filmController.addFilm(film));
         assertTrue(filmController.getAllFilms().isEmpty());
     }
 
     @Test
     void addFilmShouldAddFilm() {
-        Film film = new Film("Test film", "Description", LocalDate.of(2001, 2, 14),
-                             90);
+        Film film = new Film("Test film", "Description", LocalDate.of(2001, 2, 14), 90);
         assertDoesNotThrow(() -> filmController.addFilm(film));
         assertEquals(1, filmController.getAllFilms().size());
     }
@@ -104,23 +100,19 @@ class FilmControllerTest {
 
     @Test
     void updateFilmShouldThrowExceptionWhenFilmIdIsWrong() {
-        Film film = new Film("Test film", "Description", LocalDate.of(2001, 2, 14),
-                90);
+        Film film = new Film("Test film", "Description", LocalDate.of(2001, 2, 14), 90);
         assertDoesNotThrow(() -> filmController.addFilm(film));
 
-        Film newFilm = new Film("Test film", "Description - 1", LocalDate.of(2001, 2,
-                14), 90);
+        Film newFilm = new Film("Test film", "Description - 1", LocalDate.of(2001, 2, 14), 90);
         newFilm.setId(100000);
         assertThrows(UpdateException.class, () -> filmController.updateFilm(newFilm));
     }
 
     @Test
     void updateFilmShouldUpdateFilm() {
-        Film film = new Film("Test film", "Description", LocalDate.of(2001, 2, 14),
-                             90);
+        Film film = new Film("Test film", "Description", LocalDate.of(2001, 2, 14), 90);
         assertDoesNotThrow(() -> filmController.addFilm(film));
-        Film newFilm = new Film("Test film", "Description-1", LocalDate.of(2001, 2,
-                     14), 90);
+        Film newFilm = new Film("Test film", "Description-1", LocalDate.of(2001, 2, 14), 90);
         newFilm.setId(film.getId());
         assertDoesNotThrow(() -> filmController.updateFilm(newFilm));
         for (Film currentFilm : filmController.getAllFilms()) {
