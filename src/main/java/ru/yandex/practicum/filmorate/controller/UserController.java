b25e48ab1a8296fae91b1cfa988e6a76e26348e1
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-10 19:07:25 +0300

    refactor: заменить в UserController.java import org.springframework.web.bind.annotation.* на импорт каждой используемой аннотации refactor: проинициализировать поле UserController.users в момент его объявления delete: удвлить UserController.UserController()

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,7 +2,11 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.exception.UpdateException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -19,11 +23,7 @@ public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private static final IdGenerator idGenerator = new IdGenerator();
 
-    private final Map<Long, User> users;
-
-    public UserController() {
-        this.users = new HashMap<>();
-    }
+    private final Map<Long, User> users = new HashMap<>();
 
     @GetMapping(USERS_PATH)
     public Collection<User> getAllUsers() {
