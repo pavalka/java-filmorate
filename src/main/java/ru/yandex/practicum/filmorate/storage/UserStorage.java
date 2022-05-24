66407c0f60d1c7c6d28f17a2c588693c63498ba7
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-24 22:07:28 +0300

    feat: создать интерфейс UserStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+/**
+ * Определяет функционал хранилища пользователей.
+ */
+public interface UserStorage extends Storage<User, Long> {
+}
