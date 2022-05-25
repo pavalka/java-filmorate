9b12f91f04bb69e04b51e595c3201735876febac
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-25 22:06:49 +0300

    feat: создать метод UserStorage.getAllUsers()

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -2,8 +2,18 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.util.Collection;
+
 /**
  * Определяет функционал хранилища пользователей.
  */
 public interface UserStorage extends Storage<User, Long> {
+    /**
+     * Метод возвращает список всех пользователей, находящихся в хранилище в виде Collection<>. Если в хранилище нет ни
+     * одного пользователя, то метод вернет пустой список.
+     *
+     * @return  список всех пользователей из хранилища; если в хранилище нет ни одного пользователя, то метод вернет
+     *          пустой список.
+     */
+    Collection<User> getAllUsers();
 }
