024f38ec0b2d3aabbf2d8b70909c62026a6c3705
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-25 21:00:06 +0300

    delete: удалить класс IdSetStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/IdSetStorage.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.storage;
-
-import java.util.Set;
-
-/**
- * Описывает функционал хранилища, которое хранит набор идентификаторов (ключей) с привязкой к другому идентификатору
- * (ключу). Например, набор идентификаторов пользователей, поставивших лайки фильму с идентификатором id.
- */
-public interface IdSetStorage extends Storage<Set<Long>, Long> {}
