1f66c18d30574ad66e2c2991932a08964de0b10b
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-29 17:02:47 +0300

    feat: создать классы ElementNotFoundException

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ElementNotFoundException.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.exception;
+
+/**
+ *  Класс исключения, которое может быть сгенерировано если запрашиваемый элемент (фильм или пользователь) не найден в
+ *  хранилище.
+ */
+public class ElementNotFoundException extends Exception {
+    public ElementNotFoundException(String msg) {
+        super(msg);
+    }
+}
