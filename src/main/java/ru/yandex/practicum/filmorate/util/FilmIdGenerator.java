775dd937bf01fadd91892b17db85f7bbe64213f2
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-29 17:01:41 +0300

    feat: создать классы UserIdGenerator и FilmIdGenerator

---
+++ src/main/java/ru/yandex/practicum/filmorate/util/FilmIdGenerator.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FilmIdGenerator extends IdGenerator{
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/util/UserIdGenerator.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserIdGenerator extends IdGenerator {
+}
