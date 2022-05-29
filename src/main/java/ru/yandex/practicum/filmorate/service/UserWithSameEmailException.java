79ee419bb9c0890bfe910d661687ccb1f0fe0650
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-29 17:08:21 +0300

    feat: создать класс UserWithSameEmailException

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserWithSameEmailException.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.service;
+
+/**
+ * Класс исключения, которое может быть сгенерировано если в хранилище пользователей пытаются добавить пользователя,
+ * email которого совпадает с email другого пользователя из хранилища.
+ */
+public class UserWithSameEmailException extends Exception {
+    public UserWithSameEmailException(String msg) {
+        super(msg);
+    }
+}
