7c850cfb3aeadd6ec1e05b83d5be4a3cea606b63
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-29 16:51:32 +0300

    refactor: удалить аннотацию @ResponseStatus для классов NullArgumentException и ValidationException

--- src/main/java/ru/yandex/practicum/filmorate/exception/NullArgumentException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NullArgumentException.java
@@ -1,12 +1,8 @@
 package ru.yandex.practicum.filmorate.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * Генерируется, если аргумент метода равен null.
  */
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class NullArgumentException extends IllegalArgumentException {
     public NullArgumentException(String msg) {
         super(msg);
