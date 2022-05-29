69ba9ab31eac1c4f253399561350841a1453a38e
Pavel Kabanov <pavels.disk@yandex.ru>
2022-05-29 16:57:46 +0300

    delete: удалить генерацию исключений у методов интерфейса Storage feat: добавить метод Storage.isKeyPresent() delete: удалить метод Storage.delete()

--- src/main/java/ru/yandex/practicum/filmorate/storage/inmemorystorage/AbstractInMemoryStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/inmemorystorage/AbstractInMemoryStorage.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.inmemorystorage;
 
-import ru.yandex.practicum.filmorate.storage.AddToStorageException;
-import ru.yandex.practicum.filmorate.storage.DeleteFromStorageException;
+import ru.yandex.practicum.filmorate.exception.NullArgumentException;
 import ru.yandex.practicum.filmorate.storage.Storage;
-import ru.yandex.practicum.filmorate.storage.UpdateStorageException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,65 +11,19 @@ public abstract class AbstractInMemoryStorage<T, K> implements Storage<T, K> {
     protected final Map<K, T> storage = new HashMap<>();
 
     /**
-     * Метод добавляет item в хранилище. При возникновении ошибки в процеесе сохранения метод генерирует исключение
-     * {@link AddToStorageException}.
+     * Метод добавляет item в хранилище. Если key = null или item = null, то метод сгенерирует исключение
+     * {@link NullArgumentException}.
      *
-     * @param key  ключ, под которым будет сохранено item;
-     * @param item объект для добавления в хранилище;
+     *  @param key  ключ, под которым будет сохранено item;
+     *  @param item объект для добавления в хранилище;
      */
     @Override
-    public void add(K key, T item) throws AddToStorageException {
+    public void put(K key, T item) {
         if (key == null || item == null) {
-            throw new AddToStorageException("Объект равен null");
-        }
-
-        if (storage.containsKey(key)) {
-            throw new AddToStorageException("Объект с key = " + key + " уже есть в хранилище.");
+            throw new NullArgumentException("Объект равен null");
         }
 
         storage.put(key, item);
-    }
-
-    /**
-     * Метод обновляет в хранилище объект item. Если item не найден в хранилище или при возникновении в процессе
-     * обновления другой ошибки, метод генерирует исключение {@link UpdateStorageException}.
-     *
-     * @param key  ключ, значение под которым нужно обновить;
-     * @param item объект для обновления;
-     * @throws UpdateStorageException генерируется если объект item не найден в хранилище или, если в процессе
-     *                                обновления произошла другая ошибка.
-     */
-    @Override
-    public void update(K key, T item) throws UpdateStorageException {
-        if (key == null || item == null) {
-            throw new UpdateStorageException("Объект равен null");
-        }
-
-        if (!storage.containsKey(key)) {
-            throw new UpdateStorageException("Объект с key = " + key + " не найден в хранилище.");
-        }
-
-        storage.put(key, item);
-    }
-
-    /**
-     * Метод удаляет объект с ключом key из хранилища. Если объект с ключом key не найден в хранили или
-     * если в процессе удаления происходит другая ошибка, то метод генерирует исключение
-     * {@link DeleteFromStorageException}.
-     *
-     * @param key ключ объекта, который нужно удалить;
-     * @throws DeleteFromStorageException генерируется если объект с ключом key не найден в хранили или
-     *                                    если в процессе удаления происходит другая ошибка
-     */
-    @Override
-    public void delete(K key) throws DeleteFromStorageException {
-        if (key == null) {
-            throw new DeleteFromStorageException("Объект равен null");
-        }
-
-        if (storage.remove(key) == null) {
-            throw new DeleteFromStorageException("Объект с key = " + key + " не найден в хранилище.");
-        }
     }
 
     /**
@@ -88,6 +40,23 @@ public abstract class AbstractInMemoryStorage<T, K> implements Storage<T, K> {
             return Optional.empty();
         }
 
-        return Optional.of(storage.get(key));
+        return Optional.ofNullable(storage.get(key));
+    }
+
+    /**
+     * Метод проверяет есть ли в хранилище объект с ключом key. Если объект присутствует в хранилище, то метод вернет
+     * true. В противном случает возвращаемое значение - false.
+     *
+     * @param key ключ, который нужно проверить;
+     * @return если объект присутствует в хранилище, то метод вернет true; в противном случает возвращаемое значение
+     * - false.
+     */
+    @Override
+    public boolean isKeyPresent(K key) {
+        if (key == null) {
+            throw new NullArgumentException("Объект равен null.");
+        }
+
+        return storage.containsKey(key);
     }
 }
