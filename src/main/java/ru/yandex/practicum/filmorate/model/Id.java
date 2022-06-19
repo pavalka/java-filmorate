db575a6db648e7ee55d3fdceb5f9352ac896d967
Pavel Kabanov <pavels.disk@yandex.ru>
2022-06-19 08:16:12 +0300

    feat: Создать класс Id

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Id.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+/**
+ * Класс описывающий идентификатор некоторого объекта.
+ */
+@Data
+public class Id {
+    private long id;
+}
