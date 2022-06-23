307ad694025ec70464f8c816699648179c1eec87
Pavel Kabanov <pavels.disk@yandex.ru>
2022-06-23 20:25:12 +0300

    feat: добавить в интерфейс FriendsStorage методы addFriend(long, long), deleteFriend(long, long)

--- src/main/java/ru/yandex/practicum/filmorate/storage/FriendsStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FriendsStorage.java
@@ -6,4 +6,19 @@ import ru.yandex.practicum.filmorate.model.Friends;
  * Определяет функционал хранилища списка друзей пользователя.
  */
 public interface FriendsStorage extends Storage<Friends> {
+    /**
+     * Метод добавляет пользователя с идентификатором friendId в список друзей пользователя с идентификатором userId.
+     *
+     * @param userId   идентификатор пользователя в список друзей которого, нужно добавить друга;
+     * @param friendId  идентификатор пользователя, которго добавляем в список друзей;
+     */
+    void addFriend(long userId, long friendId);
+
+    /**
+     * Метод удаляет пользователя с идентификатором friendId из списка друзей поьзователя с идентификатором userId.
+     *
+     * @param userId    идентификатор пользователя из списка друзей которого, нужно удалить друга
+     * @param friendId  идентификатор пользователя, которого нужно удалить из списка друзей;
+     */
+    void deleteFriend(long userId, long friendId);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/inmemorystorage/InMemoryFriendsStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/inmemorystorage/InMemoryFriendsStorage.java
@@ -9,4 +9,32 @@ import ru.yandex.practicum.filmorate.storage.FriendsStorage;
  */
 @Component("inMemoryFriendsStorage")
 public class InMemoryFriendsStorage extends AbstractInMemoryStorage<Friends> implements FriendsStorage {
+    /**
+     * Метод добавляет пользователя с идентификатором friendId в список друзей пользователя с идентификатором userId.
+     *
+     * @param userId   идентификатор пользователя в список друзей которого, нужно добавить друга;
+     * @param friendId идентификатор пользователя, которго добавляем в список друзей;
+     */
+    @Override
+    public void addFriend(long userId, long friendId) {
+        Friends friends = storage.getOrDefault(userId, new Friends(userId));
+        friends.addFriend(friendId);
+        put(friends);
+    }
+
+    /**
+     * Метод удаляет пользователя с идентификатором friendId из списка друзей поьзователя с идентификатором userId.
+     *
+     * @param userId   идентификатор пользователя из списка друзей которого, нужно удалить друга
+     * @param friendId идентификатор пользователя, которого нужно удалить из списка друзей;
+     */
+    @Override
+    public void deleteFriend(long userId, long friendId) {
+        Friends friends = storage.get(userId);
+
+        if (friends != null) {
+            friends.deleteFriend(friendId);
+            put(friends);
+        }
+    }
 }
