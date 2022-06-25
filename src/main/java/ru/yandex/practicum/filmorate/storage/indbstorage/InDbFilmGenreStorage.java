4f76e5c975c782298bc79b9050d43c873313c168
Pavel Kabanov <pavels.disk@yandex.ru>
2022-06-25 07:56:13 +0300

    refactor: добавить аннотацию @Profile для класса InDbFilmGenreStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/indbstorage/InDbFilmGenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/indbstorage/InDbFilmGenreStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.indbstorage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 /**
  * Класс реализует интерфейс FilmGenreStorage и использует БД для хранения данных.
  */
+@Profile("in_db_storage")
 @Component
 public class InDbFilmGenreStorage implements FilmGenreStorage {
     private static final String REQUEST_GENRES_FOR_FILM =   "SELECT g.genre_id AS id, " +
