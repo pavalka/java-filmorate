206be1ca338773706052f294fceb4fd34a0227fe
Pavel Kabanov <pavels.disk@yandex.ru>
2022-06-25 08:04:45 +0300

    feat: добавить аннотацию @Profile для класса FilmControllerExceptionHandler

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmControllerExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +15,7 @@ import ru.yandex.practicum.filmorate.service.UserNotFoundException;
 
 import javax.validation.ConstraintViolationException;
 
+@Profile({"in_memory_storage", "in_db_storage"})
 @RestControllerAdvice(assignableTypes = {FilmController.class})
 public class FilmControllerExceptionHandler {
     private final Logger logger;
